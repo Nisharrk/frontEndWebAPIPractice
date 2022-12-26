f9d42227831935c195b5027a1406cb6c8b484439
Your Name <you@example.com>
2022-12-26 17:51:13 -0500

    finished kinda

--- src/app.js
+++ src/app.js
@@ -1,8 +1,36 @@
-const { load } = require("./users.js");
+const users = require("./users");
+const ProfileCard = require("./components/profile-card");
 
 function init() {
+  // Safe to query for DOM nodes now that window is loaded
   const main = document.querySelector("main");
-  console.log("Hello World!");
+
+  // Load all users from web API as JSON and process into DOM nodes
+  users
+    .load()
+    // We have to wait for the fetch() response's Promise to complete
+    .then((users) => {
+      // If we couldn't load any users, indicate that
+      if (!(users && users.length)) {
+        main.innerHTML = "Unable to load user data at this time.";
+        return;
+      }
+
+      // Otherwise, iterate across all the users
+      users.forEach((user) => {
+        // Extract and prepare the user properties we care about
+        const id = user.id;
+        const name = `${user.first_name} ${user.last_name}`;
+        const email = user.email;
+        const avatarUrl = user.avatar;
+
+        // Create a ProfileCard of DOM nodes for each user
+        const profileCard = new ProfileCard(id, name, email, avatarUrl);
+
+        // Append the user's ProfileCard DOM nodes to our document's main element
+        main.appendChild(profileCard.render());
+      });
+    });
 }
 
 window.onload = init;
--- src/users.js
+++ src/users.js
@@ -1,7 +1,23 @@
-const apiURL = "https://reqres.in/api";
+const apiUrl = "https://reqres.in/api";
 
 function load() {
-  console.log("load was called");
+  const url = `${apiUrl}/users`;
+
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`API returned status code ${res.status}`);
+      }
+
+      return res.json();
+    })
+    .then((results) => results.data)
+    .catch((err) => {
+      console.warn(err);
+
+      // We have no users to process, return an empty array
+      return [];
+    });
 }
 
 module.exports.load = load;
